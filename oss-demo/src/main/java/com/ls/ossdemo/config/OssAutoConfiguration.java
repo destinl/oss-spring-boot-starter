f2207e80ea000612d0b679dc5e0436a18cc41907
destinl <1374214766@qq.com>
2024-07-10 22:47:10 +0800

    修改为阿里云oss格式

--- oss-demo/src/main/java/com/ls/ossdemo/config/OssAutoConfiguration.java
+++ oss-demo/src/main/java/com/ls/ossdemo/config/OssAutoConfiguration.java
@@ -8,6 +8,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.ls.ossdemo.core.OssProperties;
 import com.ls.ossdemo.core.OssTemplate;
 import com.ls.ossdemo.server.OssTemplateImpl;
@@ -34,20 +35,30 @@ public class OssAutoConfiguration {
     //@ConditionalOnMissingBean： 修饰bean的一个注解，当你的bean被注册之后，注册相同类型的bean，就不会成功，它会保证你的bean只有一个，
     //即你的实例只有一个。多个会报错。
     public AmazonS3 ossClient(OssProperties ossProperties) {
-        // 客户端配置，主要是全局的配置信息,有很多的配置，有的指定了默认值有的没有
-        ClientConfiguration clientConfiguration = new ClientConfiguration();
-        clientConfiguration.setMaxConnections(ossProperties.getMaxConnections());
-        // url以及region配置
-        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
-                ossProperties.getEndpoint(), ossProperties.getRegion());
-        // 凭证配置
-        AWSCredentials awsCredentials = new BasicAWSCredentials(ossProperties.getAccessKey(),
-                ossProperties.getSecretKey());
-        AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
-        // build amazonS3Client客户端
-        return AmazonS3Client.builder().withEndpointConfiguration(endpointConfiguration)
-                .withClientConfiguration(clientConfiguration).withCredentials(awsCredentialsProvider)
-                .disableChunkedEncoding().withPathStyleAccessEnabled(ossProperties.getPathStyleAccess()).build();
+//        // 客户端配置，主要是全局的配置信息,有很多的配置，有的指定了默认值有的没有
+//        ClientConfiguration clientConfiguration = new ClientConfiguration();
+//        clientConfiguration.setMaxConnections(ossProperties.getMaxConnections());
+//        // url以及region配置
+//        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
+//                ossProperties.getEndpoint(), ossProperties.getRegion());
+//        // 凭证配置
+//        AWSCredentials awsCredentials = new BasicAWSCredentials(ossProperties.getAccessKey(),
+//                ossProperties.getSecretKey());
+//        AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
+//        // build amazonS3Client客户端
+//        return AmazonS3Client.builder().withEndpointConfiguration(endpointConfiguration)
+//                .withClientConfiguration(clientConfiguration).withCredentials(awsCredentialsProvider)
+//                .disableChunkedEncoding().withPathStyleAccessEnabled(ossProperties.getPathStyleAccess()).build();
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("此处修改为阿里云accessKeyId"
+                        , "此处修改为accessKeySecret")))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                        "此处修改为Endpoint",//https://oss-cn-guangzhou.aliyuncs.com
+                        ""))
+                .withPathStyleAccessEnabled(false)
+                .withChunkedEncodingDisabled(true)
+                .build();
+        return s3Client;
     }
 
     @Bean
